ef154379e0053d4be1395bf4dac9d035df2939ac
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2020-02-17 12:25:22 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ const btn = document.querySelector('#btn');
 const tip = document.querySelector('#tip');
 const rate = document.querySelector('#rate');
 const result = document.querySelector('#result');
+const tax = document.querySelector('#tax');
 btn.addEventListener("click",calculate);
 
 
 function calculate()
 {
-    const tax = 5.50;
+    const taxp = 5.50;
+    const tax=(taxp*total)/100;
     
     tip.textContent='Tip: $' + ((rate.value*total.value)/100.0);
     calctotal = (rate.value*total.value/100.0)+Number(total.value)+tax;
