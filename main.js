0c2524d5595a0650033166429fc6799baf5ee14c
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2020-02-17 12:27:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ btn.addEventListener("click",calculate);
 function calculate()
 {
     const taxp = 5.50;
-    const tax=(taxp*total)/100;
+    const tax=(taxp*total.value)/100;
     
     tip.textContent='Tip: $' + ((rate.value*total.value)/100.0);
     calctotal = (rate.value*total.value/100.0)+Number(total.value)+tax;
